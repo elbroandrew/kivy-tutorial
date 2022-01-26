a8e622e9e2b3f55f8d339fc9a1d5031211a323bb
Andrew <elbroandrew@gmail.com>
2022-01-26 18:43:22 +1000

    change orientation stak layout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cgitb import text
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
@@ -13,7 +14,7 @@ class StackLayoutExample(StackLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		for i in range(0, 10):
-			b = Button(text="{}".format(i + 1), size_hint=(0.2, 0.2))
+			b = Button(text="{}".format(i + 1), size_hint=(None, None), size=(dp(100), dp(100)))
 			self.add_widget(b)
 
 
