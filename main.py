f8dedf11a2563a2769e04d2f48a44b75cad3d263
Andrew <elbroandrew@gmail.com>
2022-01-26 20:49:02 +1000

    передаю self в kv

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ class WidgetsExample(GridLayout):
 		self.count += 1
 		self.my_text = str(self.count)
 	
-	def on_toggle_button_state(self):
-		print('toggle state')
+	def on_toggle_button_state(self, widget):
+		print('toggle state' + widget.state)
 		
 
 
