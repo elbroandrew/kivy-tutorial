2d8cbe8f1502d110baa0105fcbbd179d5b8d2baf
Andrew <elbroandrew@gmail.com>
2024-04-25 21:41:16 +1000

    show hide text on toggle

--- main.py
+++ main.py
@@ -5,26 +5,23 @@ from kivy.uix.gridlayout import GridLayout
 
 
 class WidgetsExample(GridLayout):
-	count = 1
+	answer = "text text text text text text text text text"
 	my_text = StringProperty("answer")
-	count_enabled = BooleanProperty(False)
 	
-
 
 	def on_button_click(self):
-		if self.count_enabled:
-			self.my_text = str(self.count)
-	
+		print("next definition")
+		self.my_text = ''
+    
 	def on_toggle_button_state(self, widget):
 		if widget.state == "normal":
 			widget.text = "show answer"
-			self.count_enabled = False
+			self.my_text = ''
 		else:      
 			widget.text = "hide answer"
-			self.count_enabled = True
-	
+			self.my_text = self.answer
 
-		
+
 
 class TheLabApp(App):
     pass 
