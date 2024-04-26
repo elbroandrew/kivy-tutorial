1cb084460b3fe3c750e9775b48003f787ca660b7
Andrew <elbroandrew@gmail.com>
2024-04-27 08:59:05 +1000

    sqlite3 db context mgr

--- sql_conn.py
+++ sql_conn.py
@@ -1,21 +1,38 @@
 import sqlite3
 
-conn = sqlite3.connect(':memory:')
 
-c = conn.cursor()
-
-c.execute('''CREATE TABLE IF NOT EXISTS websites
+class Database():
+    
+    
+    def __init__(self):
+        self.conn = sqlite3.connect(':memory:')
+        self.c = self.conn.cursor()
+    
+    
+    def __enter__(self):
+        return self
+    
+    
+    def __exit__(self, ext_type, exc_value, traceback):
+        self.c.close()
+        self.conn.commit()
+        self.conn.close()
+        
+        
+    def create_table(self):
+        self.c.execute('''CREATE TABLE IF NOT EXISTS websites
           (title TEXT, url TEXT)''')
+        
 
-c.execute('''INSERT INTO websites (title, url) VALUES ('google', 'https://www.google.com')''')
-
-conn.commit()
-
-c.execute('SELECT * FROM websites')
-websites = c.fetchall()
-
-for website in websites:
-    print(website)
+    def insert(self):
+        self.c.execute('''INSERT INTO websites (title, url) VALUES ('google', 'https://www.google.com')''')
 
 
-conn.close()
+    def fetch_all(self):
+        
+        self.c.execute('SELECT * FROM websites')
+        websites = self.c.fetchall()
+
+        return websites
+
+
